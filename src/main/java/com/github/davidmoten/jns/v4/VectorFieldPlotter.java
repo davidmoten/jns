9e5cac70c815f552f9ddc78824ef4b6b654c7457
Dave Moten <davidmoten@gmail.com>
2023-06-05 20:57:18 +1000

    plot dev

--- src/main/java/com/github/davidmoten/jns/v4/VectorFieldPlotter.java
+++ src/main/java/com/github/davidmoten/jns/v4/VectorFieldPlotter.java
@@ -7,7 +7,7 @@ import org.jzy3d.colors.ColorMapper;
 import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.maths.Range;
 import org.jzy3d.plot3d.builder.Builder;
-import org.jzy3d.plot3d.builder.Mapper;
+import org.jzy3d.plot3d.builder.Func3D;
 import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
 import org.jzy3d.plot3d.primitives.Shape;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
@@ -21,20 +21,16 @@ public class VectorFieldPlotter {
         System.setProperty("jogl.disable.openglcore", "false");
 
         // Define a function to plot
-        Mapper mapper = new Mapper() {
-            public double f(double x, double y) {
-                return 5 * Math.sin(x / 10) / (1 + Math.exp(-y/35));
-            }
-        };
-
-        // Define range and precision for the function to plot
-        Range range = new Range(-100, 100);
-        int steps = 50;
+        Func3D func = new Func3D((x, y) -> Math.log(x) * Math.sin(Math.log(x) * Math.log(y)));
         
-        Range functionRange = new Range(-5, 5);
+        // Define range and precision for the function to plot
+        Range range = new Range(-10, 10);
+        int steps = 50;
+
+        Range functionRange = new Range(-2, 2);
 
         // Create a surface drawing that function
-        Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps), mapper);
+        Shape surface = Builder.buildOrthonormal(new OrthonormalGrid(range, steps), func);
         surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), functionRange));
         surface.setFaceDisplayed(true);
         surface.setWireframeDisplayed(true);
